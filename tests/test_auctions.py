e218e96deffbc9398b8d15ec99cfa19c8ad428cf
bargst <martin@makerdao.com>
2018-11-19 10:49:40 +0100

    Rewrite of TestFlipper with pytest.fixture()

--- tests/test_auctions.py
+++ tests/test_auctions.py
@@ -14,7 +14,9 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from pymaker.numeric import Ray
+import pytest
+from pymaker.dss import Collateral
+
 from web3 import HTTPProvider
 from web3 import Web3
 
@@ -22,129 +24,161 @@ from pymaker import Address, Wad, Contract
 from pymaker.approval import directly
 from pymaker.auctions import Flipper, Flapper, Flopper
 from pymaker.auth import DSGuard
+from pymaker.deployment import DssDeployment
+from pymaker.numeric import Ray
 from pymaker.token import DSToken
-from tests.helpers import time_travel_by
+
+from tests.helpers import time_travel_by, snapshot, reset
+
+
+@pytest.fixture(scope="session")
+def web3():
+    web3 = Web3(HTTPProvider("http://localhost:8555"))
+    web3.eth.defaultAccount = web3.eth.accounts[0]
+    return web3
+
+@pytest.fixture(scope="session")
+def d(web3):
+    deployment = DssDeployment.deploy(web3=web3, debt_ceiling=Wad.from_number(1000000))
+    for c in deployment.collaterals:
+        assert c.gem.mint(Wad.from_number(1000)).transact()
+    return deployment
+
+
+@pytest.fixture(scope="session")
+def dai(d: DssDeployment):
+    return d.dai
+
+
+@pytest.fixture(scope="session")
+def c(d: DssDeployment):
+    return d.collaterals[0]
+
+
+@pytest.fixture(scope="session")
+def gem(c: Collateral):
+    return c.gem
+
+
+@pytest.fixture(scope="session")
+def flipper(c: Collateral):
+    return c.flipper
 
 
 class TestFlipper:
-    def setup_method(self):
-        self.web3 = Web3(HTTPProvider("http://localhost:8555"))
-        self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
-        self.our_address = Address(self.web3.eth.defaultAccount)
-        self.other_address_1 = Address(self.web3.eth.accounts[1])
-        self.other_address_2 = Address(self.web3.eth.accounts[2])
+    # def setup_method(self):
+    #     self.web3 = Web3(HTTPProvider("http://localhost:8555"))
+    #     self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
+    #     self.our_address = Address(self.web3.eth.defaultAccount)
+    #     self.other_address_1 = Address(self.web3.eth.accounts[1])
+    #     self.other_address_2 = Address(self.web3.eth.accounts[2])
+    #
+    #     self.dai = DSToken.deploy(self.web3, 'DAI')
+    #
+    #     # we need a GemLike version of DSToken with push(bytes32, uint function)
+    #     self.gem_addr = Contract._deploy(self.web3, Contract._load_abi(__name__, 'abi/GemMock.abi'), Contract._load_bin(__name__, 'abi/GemMock.bin'), [b'ABC'])
+    #     self.gem = DSToken(web3=self.web3, address=self.gem_addr)
+    #
+    #     self.flipper = Flipper.deploy(self.web3, self.dai.address, self.gem.address)
+    #
+    #     # Set allowance to allow flipper to move dai and gem
+    #     # With full deployment kick is only done by Cat via flip() which take care of allowance via gem.hope()
+    #     self.gem.approve(self.flipper.address).transact()
+    #     self.gem.approve(self.flipper.address).transact(from_address=self.other_address_1)
+    #     self.dai.approve(self.flipper.address).transact()
 
-        self.dai = DSToken.deploy(self.web3, 'DAI')
+    def test_era(self, flipper):
+        assert flipper.era() > 1000000
 
-        # we need a GemLike version of DSToken with push(bytes32, uint function)
-        self.gem_addr = Contract._deploy(self.web3, Contract._load_abi(__name__, 'abi/GemMock.abi'), Contract._load_bin(__name__, 'abi/GemMock.bin'), [b'ABC'])
-        self.gem = DSToken(web3=self.web3, address=self.gem_addr)
+    def test_beg(self, flipper):
+        assert flipper.beg() == Ray.from_number(1.05)
 
-        self.flipper = Flipper.deploy(self.web3, self.dai.address, self.gem.address)
+    def test_ttl(self, flipper):
+        assert flipper.ttl() == 3*60*60  # 3 hours
 
-        # Set allowance to allow flipper to move dai and gem
-        # With full deployment kick is only done by Cat via flip() which take care of allowance via gem.hope()
-        self.gem.approve(self.flipper.address).transact()
-        self.gem.approve(self.flipper.address).transact(from_address=self.other_address_1)
-        self.dai.approve(self.flipper.address).transact()
+    def test_tau(self, flipper):
+        assert flipper.tau() == 2*24*60*60  # 2 days
 
-    def dai_balance(self, address: Address) -> Wad:
-        assert(isinstance(address, Address))
-        return self.dai.balance_of(address)
-
-    def dai_mint(self, amount: Wad):
-        assert(isinstance(amount, Wad))
-        assert self.dai.mint(amount).transact()
-
-    def gem_balance(self, address: Address) -> Wad:
-        assert(isinstance(address, Address))
-        return self.gem.balance_of(address)
-
-    def test_era(self):
-        assert self.flipper.era() > 1000000
-
-    def test_beg(self):
-        assert self.flipper.beg() == Ray.from_number(1.05)
-
-    def test_ttl(self):
-        assert self.flipper.ttl() == 3*60*60  # 3 hours
-
-    def test_tau(self):
-        assert self.flipper.tau() == 2*24*60*60  # 2 days
-
-    def test_read(self):
+    def test_read(self, web3, gem: DSToken, flipper):
         # given
-        assert self.gem.mint(Wad.from_number(100)).transact()
-        assert self.gem_balance(self.our_address) == Wad.from_number(100)
+        our_address = Address(web3.eth.accounts[0])
+        other_address_1 = Address(web3.eth.accounts[1])
+        other_address_2 = Address(web3.eth.accounts[2])
+
+        assert gem.mint(Wad.from_number(100)).transact()
+        assert gem.balance_of(our_address) >= Wad.from_number(100)
 
         # when
-        assert self.flipper.kick(urn=self.other_address_1,
-                                 gal=self.other_address_2,
-                                 tab=Wad.from_number(5000),
-                                 lot=Wad.from_number(100),
-                                 bid=Wad.from_number(100)).transact()
+        assert flipper.kick(urn=other_address_1,
+                            gal=other_address_2,
+                            tab=Wad.from_number(5000),
+                            lot=Wad.from_number(100),
+                            bid=Wad.from_number(100)).transact()
 
         # then
-        assert self.flipper.kicks() == 1
+        assert flipper.kicks() == 1
         # and
-        auction = self.flipper.bids(1)
-        assert auction.lad == self.other_address_1
-        assert auction.gal == self.other_address_2
+        auction = flipper.bids(1)
+        assert auction.lad == other_address_1
+        assert auction.gal == other_address_2
         assert auction.bid == Wad.from_number(100)
         assert auction.lot == Wad.from_number(100)
         assert auction.tab == Wad.from_number(5000)
-        assert auction.guy == self.our_address
+        assert auction.guy == our_address
         assert auction.tic == 0
         assert auction.end > 0
 
-    def test_scenario(self):
+    def test_scenario(self, web3, gem: DSToken, dai: DSToken, flipper):
         # given
-        self.dai_mint(Wad.from_number(100000))
-        assert self.gem.mint(Wad.from_number(100)).transact()
-        assert self.gem.transfer(self.other_address_1, Wad.from_number(100)).transact()
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000)
+        our_address = Address(web3.eth.accounts[0])
+        other_address_1 = Address(web3.eth.accounts[1])
+
+        assert dai.mint(Wad.from_number(100000)).transact()
+        assert gem.mint(Wad.from_number(100)).transact()
+        assert gem.transfer(other_address_1, Wad.from_number(100)).transact()
+        assert dai.balance_of(our_address) == Wad.from_number(100000)
 
         # when
-        self.flipper.kick(urn=self.other_address_1,
-                          gal=self.other_address_1,
-                          tab=Wad.from_number(5000),
-                          lot=Wad.from_number(100),
-                          bid=Wad.from_number(1000)).transact(from_address=self.other_address_1)
+        flipper.kick(urn=other_address_1,
+                     gal=other_address_1,
+                     tab=Wad.from_number(5000),
+                     lot=Wad.from_number(100),
+                     bid=Wad.from_number(1000)).transact(from_address=other_address_1)
         # then
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000)
-        assert self.dai_balance(self.other_address_1) == Wad.from_number(0)
-        assert self.gem_balance(self.our_address) == Wad.from_number(0)
-        assert self.gem_balance(self.other_address_1) == Wad.from_number(0)
+        assert dai.balance_of(our_address) == Wad.from_number(100000)
+        assert dai.balance_of(other_address_1) == Wad.from_number(0)
+        assert gem.balance_of(our_address) == Wad.from_number(0)
+        assert gem.balance_of(other_address_1) == Wad.from_number(0)
 
         # when
-        self.flipper.tend(1, Wad.from_number(100), Wad.from_number(2000)).transact()
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000) - Wad.from_number(2000)
-        assert self.dai_balance(self.other_address_1) == Wad.from_number(2000)
-        assert self.gem_balance(self.our_address) == Wad.from_number(0)
-        assert self.gem_balance(self.other_address_1) == Wad.from_number(0)
+        flipper.tend(1, Wad.from_number(100), Wad.from_number(2000)).transact()
+        assert dai.balance_of(our_address) == Wad.from_number(100000) - Wad.from_number(2000)
+        assert dai.balance_of(other_address_1) == Wad.from_number(2000)
+        assert gem.balance_of(our_address) == Wad.from_number(0)
+        assert gem.balance_of(other_address_1) == Wad.from_number(0)
 
         # when
-        self.flipper.tend(1, Wad.from_number(100), Wad.from_number(5000)).transact()
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000) - Wad.from_number(5000)
-        assert self.dai_balance(self.other_address_1) == Wad.from_number(5000)
-        assert self.gem_balance(self.our_address) == Wad.from_number(0)
-        assert self.gem_balance(self.other_address_1) == Wad.from_number(0)
+        flipper.tend(1, Wad.from_number(100), Wad.from_number(5000)).transact()
+        assert dai.balance_of(our_address) == Wad.from_number(100000) - Wad.from_number(5000)
+        assert dai.balance_of(other_address_1) == Wad.from_number(5000)
+        assert gem.balance_of(our_address) == Wad.from_number(0)
+        assert gem.balance_of(other_address_1) == Wad.from_number(0)
 
         # when
-        self.flipper.dent(1, Wad.from_number(80), Wad.from_number(5000)).transact()
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000) - Wad.from_number(5000)
-        assert self.dai_balance(self.other_address_1) == Wad.from_number(5000)
-        assert self.gem_balance(self.our_address) == Wad.from_number(0)
-        assert self.gem_balance(self.other_address_1) == Wad.from_number(20)
+        flipper.dent(1, Wad.from_number(80), Wad.from_number(5000)).transact()
+        assert dai.balance_of(our_address) == Wad.from_number(100000) - Wad.from_number(5000)
+        assert dai.balance_of(other_address_1) == Wad.from_number(5000)
+        assert gem.balance_of(our_address) == Wad.from_number(0)
+        assert gem.balance_of(other_address_1) == Wad.from_number(20)
 
-        time_travel_by(self.web3, 60*60*24*8)
+        time_travel_by(web3, 60*60*24*8)
 
         # when
-        self.flipper.deal(1).transact()
-        assert self.dai_balance(self.our_address) == Wad.from_number(100000) - Wad.from_number(5000)
-        assert self.dai_balance(self.other_address_1) == Wad.from_number(5000)
-        assert self.gem_balance(self.our_address) == Wad.from_number(80)
-        assert self.gem_balance(self.other_address_1) == Wad.from_number(20)
+        flipper.deal(1).transact()
+        assert dai.balance_of(our_address) == Wad.from_number(100000) - Wad.from_number(5000)
+        assert dai.balance_of(other_address_1) == Wad.from_number(5000)
+        assert gem.balance_of(our_address) == Wad.from_number(80)
+        assert gem.balance_of(other_address_1) == Wad.from_number(20)
 
 
 class TestFlapper:
